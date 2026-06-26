7ab864f6f42e196d9b4b540c05f0dbf89afdf51b
FeatureSmith25 <hardiksachan25@gmail.com>
2026-06-26 10:40:48 +0530

    2D arary practice problems

--- Sorting_algo/merging_soted_array_using_magic.java
+++ Sorting_algo/merging_soted_array_using_magic.java
@@ -12,11 +12,15 @@ public class merging_soted_array_using_magic {
     public static void merge_sort(int[] arr){
         int n=arr.length;
         if(n==1) return;
+
         int[] a=new int[n/2];
         int[] b=new int[n-n/2];
+
         int idx=0;
+        
         for(int i=0;i<a.length;i++) a[i]=arr[idx++];
         for(int i=0;i<b.length;i++) b[i]=arr[idx++];
+        
         merge_sort(a);
         merge_sort(b);
         merge(a, b, arr);
